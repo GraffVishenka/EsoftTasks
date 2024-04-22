534c9b3f76989c83abad10db4deb8564fb61ce8d
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-04-22 09:02:39 +0500

    c

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,14 +4,17 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      credentials: true,
+      origin:
+        "https://esoft-tasks.vercel.app",
+    },
+  });
 
   app.use(cookieParser());
 
-  app.enableCors({
-    credentials: true,
-    origin: "https://esoft-tasks.vercel.app",
-  });
+  //app.enableCors({ credentials: true, origin: process.env.CLIENT_URL });
 
   const config = new DocumentBuilder()
     .setTitle("TaskManager")
