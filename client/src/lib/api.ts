b93d6b5bd04eb07797426cc44f712ff6c3971ac9
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-04-22 09:31:15 +0500

    f

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = `https://taskserveresoft.netlify.app`
+export const API_URL = `https://esoftserver.onrender.com`
 
 export const $api = axios.create({
   withCredentials:true,
