dc26225cb7bc05bf17d7298159206599b58a03ba
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-04-22 07:28:26 +0500

    f

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{ cors: {credentials:true, origin: "http://localhost:3000"} });
+  const app = await NestFactory.create(AppModule,{ cors: {credentials:true, origin: "esoft-tasks-ibhgdoidz-graffvishenkas-projects.vercel.app"} });
 
   app.use(cookieParser())
   
